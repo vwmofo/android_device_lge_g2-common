729a43f9a34fe4b104bf95ae34ab346e48d463b2
Griffin Millender <griffinn.millender@gmail.com>
2014-11-23 11:48:54 -0600

    replace loki with open bump

--- g2.mk
+++ g2.mk
@@ -117,8 +117,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.sf.lcd_density=480 \
-	ro.opengles.version=196608 \
-	ro.loki_enabled=1
+	ro.opengles.version=196608
 
 # Audio Configuration
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -231,12 +230,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/configs/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
-
-# This hw ships locked, work around it with loki
-PRODUCT_PACKAGES += \
-        loki.sh \
-        loki_tool_static_g2 \
-        recovery-transform.sh
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
